40dfce2588dfd54124d40cf468d41a1e7dd01be3
mcjclaude <mcjclaude2002@gmail.com>
2023-02-28 23:02:54 -0800

    gy

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the alphabet at reverse
+  * main - Prints a serie of numbers with commas
   *
-  * Return: Always (Success)
+  * Return: Always (Success);
   */
 int main(void)
 {
-	char c;
+	int c;
 
-	for (c = 'z'; c >= 'a'; c--)
+	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
