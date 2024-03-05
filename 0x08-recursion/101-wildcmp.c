2a1efea3ab460634412bdb79fff68f755d05fd05
mcjclaude <mcjclaude2002@gmail.com>
2024-03-05 15:54:48 +0300

    better way

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,48 +1,20 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * wildcmp - Compare two strings using wildcard characters.
- *
- * This function compares two strings, 'mot' and 'a_comparer'
- *
- * @mot: The source string to compare.
- * @a_comparer: The string to compare with, may contain '*'²:wq
- *
- *
- * Return: 1 if the strings match, 0 otherwise.
+ * wildcmp - compares two strings.
+ * @s1: string 1.
+ * @s2: string 2. It can contains a * as a special character.
+ * Return: 1 if s is palindrome, 0 if not.
  */
-
-
-int wildcmp(char *mot, char *a_comparer)
+int wildcmp(char *s1, char *s2)
 {
-	int ctr = 0, flag = 0;
-
-	if (*mot == '\0' && *a_comparer == '\0')
-		return (1);
-
-	if (*mot == *a_comparer)
-		return (wildcmp(++mot, ++a_comparer));
-
-	if (*a_comparer == '*')
-	{
-		while (*a_comparer == '*')
-			a_comparer++;
-
-		if (*a_comparer == '\0')
-			return (1);
-
-		while (*(mot + ctr) != '\0')
-		{
-			if (*(mot + ctr) == *a_comparer)
-				flag = ctr;
-			ctr++;
-		}
-		if (flag > 0)
-			mot = mot + flag;
-		return (wildcmp(mot, a_comparer));
-	}
-
-	return (0);
+if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+return (0);
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+if (*s2 == '*')
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+return (0);
 }
